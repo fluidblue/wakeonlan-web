e4c05e338ec8d9682ecfd5b43536acf642bb7248
fluidblue <fluidblue@users.noreply.github.com>
2021-07-31 21:42:50 +0200

    Added WakeOnLan interface

---
+++ src/WakeOnLan.ts
@@ -0,0 +1,24 @@
+const WAKE_ON_LAN_PORT: number = 9;
+const IP_BROADCAST_ADDRESS: string = "255.255.255.255";
+
+type WakeOnLanOptions = {
+	port?: number;
+	address?: string;
+};
+
+const wakeOnLanOptionsDefault: WakeOnLanOptions = {
+	port: WAKE_ON_LAN_PORT,
+	address: IP_BROADCAST_ADDRESS
+};
+
+interface WakeOnLan {
+	wake(options?: WakeOnLanOptions, callback?: (error: Error | null) => void): void;
+}
+
+class NativeNodeJs implements WakeOnLan {
+	wake(options?: WakeOnLanOptions, callback?: (error: Error | null) => void): void {
+		console.log(options);
+	}
+}
+
+let nativeNodeJs: NativeNodeJs = new NativeNodeJs();
