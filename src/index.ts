d2a1d7c498c34835cef841ea17f939d4387c84f8
fluidblue <fluidblue@users.noreply.github.com>
2021-07-31 17:58:21 +0200

    Replaced quotes

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,15 @@ const MAGIC_PACKET_OFFSET: number = 6;
 const MAGIC_PACKET_MAC_REPETITIONS: number = 16;
 const MAGIC_PACKET_LENGTH: number = MAGIC_PACKET_OFFSET + MAGIC_PACKET_MAC_REPETITIONS * MAC_ADDR_LENGTH;
 
-const destinationMacAddress: Uint8Array = Buffer.alloc(MAC_ADDR_LENGTH, '001FD0DB55A2', 'hex');
+const destinationMacAddress: Uint8Array = Buffer.alloc(MAC_ADDR_LENGTH, "001FD0DB55A2", "hex");
 const destinationPort: number = 9;
 
-const broadcastAddress: string = '255.255.255.255'; // TODO: When powered on, may use 192.168.188.22 for testing
+const broadcastAddress: string = "255.255.255.255"; // TODO: When powered on, may use 192.168.188.22 for testing
 
-type Protocol = 'udp4' | 'udp6';
+type Protocol = "udp4" | "udp6";
 
 function createMagicPacket(macAddress: Uint8Array): Uint8Array {
-	let magicPacket: Uint8Array = Buffer.alloc(MAGIC_PACKET_LENGTH, 'FF', 'hex');
+	let magicPacket: Uint8Array = Buffer.alloc(MAGIC_PACKET_LENGTH, "FF", "hex");
 	for (let i = 0; i < MAGIC_PACKET_MAC_REPETITIONS; i++) {
 		for (let j = 0; j < MAC_ADDR_LENGTH; j++) {
 			magicPacket[MAGIC_PACKET_OFFSET + i * MAC_ADDR_LENGTH + j] = macAddress[j]
@@ -27,7 +27,7 @@ function createMagicPacket(macAddress: Uint8Array): Uint8Array {
 
 function wake(port: number, address?: string, protocol?: Protocol) {
 	if (!protocol) {
-		protocol = 'udp4'; // TODO: Infer default protocol from socket creation, if possible
+		protocol = "udp4"; // TODO: Infer default protocol from socket creation, if possible
 	}
 	if (!address) {
 		address = broadcastAddress; // TODO: Adapt to IPv6
@@ -47,7 +47,7 @@ function wake(port: number, address?: string, protocol?: Protocol) {
 			if (err) {
 				console.log(err);
 			} else {
-				console.log('Message has been sent.')
+				console.log("Message has been sent.")
 			}
 			socket.close();
 		});
