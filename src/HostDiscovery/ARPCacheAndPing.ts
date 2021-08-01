0d3ccf9d190a53072b2160a405c7977e214a2836
fluidblue <fluidblue@users.noreply.github.com>
2021-08-01 14:15:15 +0200

    Member functions

--- src/HostDiscovery/ARPCacheAndPing.ts
+++ src/HostDiscovery/ARPCacheAndPing.ts
@@ -1,82 +1,6 @@
 import { HostDiscovery, IPNetwork } from "./HostDiscovery"
 import { exec } from "child_process";
 
-function getBytesFromIP(ip: string): Uint8Array {
-	// Assume IPv4 address
-	const parts: string[] = ip.split(".");
-	const buffer: Uint8Array = Buffer.alloc(4);
-	for (let i = 0; i < 4; i++) {
-		buffer[i] = parseInt(parts[i]);
-	}
-	return buffer;
-}
-
-function getNumberFromIP(ip: string): number {
-	// Assume IPv4 address
-	const parts: string[] = ip.split(".");
-	let result: number = 0;
-	for (let i = 0; i < 4; i++) {
-		result = (result | (parseInt(parts[i]) << ((3 - i) * 8))) >>> 0;
-	}
-	return result;
-}
-
-function getIPFromNumber(ip: number): string {
-	const result: number[] = [0, 0, 0, 0];
-	for (let i = 0; i < 4; i++) {
-		const shift = ((3 - i) * 8);
-		result[i] = (ip & (0xFF << shift)) >>> shift;
-	}
-	return result.join(".");
-}
-
-function getSubnetMask(subnet: number): number {
-	const subnetMask = (0xFFFFFFFF << (32 - subnet)) >>> 0;
-
-	console.log("\nsubnetMask:");
-	console.log(subnetMask);
-	console.log(getIPFromNumber(subnetMask));
-
-	return subnetMask;
-}
-
-function getCleanNetworkAddress(ip: number, subnetMask: number): number {
-	return (ip & subnetMask) >>> 0;
-}
-
-function getFirstAddress(ipNetwork: number, subnet: number): number {
-	const subnetMask = getSubnetMask(subnet);
-	ipNetwork = getCleanNetworkAddress(ipNetwork, subnetMask);
-
-	// If subnet == 32, the only host address is the net address.
-	// If subnet == 31, the first host address is the net address.
-	// If subnet <= 30, the first host address is the IP address after the net address.
-	let ipFirst = ipNetwork;
-	if (subnet <= 30) {
-		ipFirst++;
-	}
-
-	return ipFirst;
-}
-
-function getLastAddress(ipNetwork: number, subnet: number): number {
-	const subnetMask = getSubnetMask(subnet);
-	ipNetwork = getCleanNetworkAddress(ipNetwork, subnetMask);
-
-	const subnetMaskInverted = (~subnetMask) >>> 0;
-
-	// If subnet == 32, the only host address is the net address.
-	// If subnet == 31, the last host address is the last address in the address space.
-	// If subnet <= 30, the last host address is the IP address before the last address
-	// in the address space (which is the broadcast address).
-	let ipLast = (ipNetwork | subnetMaskInverted) >>> 0;
-	if (subnet <= 30) {
-		ipLast--;
-	}
-
-	return ipLast;
-}
-
 class ARPCacheAndPing implements HostDiscovery {
 	discover(
 		ipSubnet: IPNetwork,
@@ -91,27 +15,27 @@ class ARPCacheAndPing implements HostDiscovery {
 		console.log("\nipSubnet:")
 		console.log(ipSubnet.prefix);
 
-		const ipNetwork: number = getNumberFromIP(ipSubnet.ip);
+		const ipNetwork: number = this.getNumberFromIP(ipSubnet.ip);
 		console.log("\nipNetwork:")
 		console.log(ipNetwork);
-		console.log(getIPFromNumber(ipNetwork));
+		console.log(this.getIPFromNumber(ipNetwork));
 
-		const ipFirst: number = getFirstAddress(ipNetwork, ipSubnet.prefix);
+		const ipFirst: number = this.getFirstAddress(ipNetwork, ipSubnet.prefix);
 		console.log("\nipStart:")
 		console.log(ipFirst);
-		console.log(getIPFromNumber(ipFirst));
+		console.log(this.getIPFromNumber(ipFirst));
 
-		const ipLast: number = getLastAddress(ipNetwork, ipSubnet.prefix);
+		const ipLast: number = this.getLastAddress(ipNetwork, ipSubnet.prefix);
 		console.log("\nipEnd:")
 		console.log(ipLast);
-		console.log(getIPFromNumber(ipLast));
+		console.log(this.getIPFromNumber(ipLast));
 
 		const totalIPs = ipLast - ipFirst + 1;
 
 		let i = 0;
 		for (let ip = ipFirst; ip <= ipLast; ip++) {
 			// TODO
-			console.log(getIPFromNumber(ip));
+			console.log(this.getIPFromNumber(ip));
 
 			i++;
 			callbackProgress(i, totalIPs);
@@ -141,6 +65,82 @@ class ARPCacheAndPing implements HostDiscovery {
 			}
 			callback(null, stdout);
 		});
+	}
+
+	function getBytesFromIP(ip: string): Uint8Array {
+		// Assume IPv4 address
+		const parts: string[] = ip.split(".");
+		const buffer: Uint8Array = Buffer.alloc(4);
+		for (let i = 0; i < 4; i++) {
+			buffer[i] = parseInt(parts[i]);
+		}
+		return buffer;
+	}
+
+	function getNumberFromIP(ip: string): number {
+		// Assume IPv4 address
+		const parts: string[] = ip.split(".");
+		let result: number = 0;
+		for (let i = 0; i < 4; i++) {
+			result = (result | (parseInt(parts[i]) << ((3 - i) * 8))) >>> 0;
+		}
+		return result;
+	}
+
+	function getIPFromNumber(ip: number): string {
+		const result: number[] = [0, 0, 0, 0];
+		for (let i = 0; i < 4; i++) {
+			const shift = ((3 - i) * 8);
+			result[i] = (ip & (0xFF << shift)) >>> shift;
+		}
+		return result.join(".");
+	}
+
+	function getSubnetMask(subnet: number): number {
+		const subnetMask = (0xFFFFFFFF << (32 - subnet)) >>> 0;
+
+		console.log("\nsubnetMask:");
+		console.log(subnetMask);
+		console.log(this.getIPFromNumber(subnetMask));
+
+		return subnetMask;
+	}
+
+	function getCleanNetworkAddress(ip: number, subnetMask: number): number {
+		return (ip & subnetMask) >>> 0;
+	}
+
+	function getFirstAddress(ipNetwork: number, subnet: number): number {
+		const subnetMask = this.getSubnetMask(subnet);
+		ipNetwork = this.getCleanNetworkAddress(ipNetwork, subnetMask);
+
+		// If subnet == 32, the only host address is the net address.
+		// If subnet == 31, the first host address is the net address.
+		// If subnet <= 30, the first host address is the IP address after the net address.
+		let ipFirst = ipNetwork;
+		if (subnet <= 30) {
+			ipFirst++;
+		}
+
+		return ipFirst;
+	}
+
+	function getLastAddress(ipNetwork: number, subnet: number): number {
+		const subnetMask = this.getSubnetMask(subnet);
+		ipNetwork = this.getCleanNetworkAddress(ipNetwork, subnetMask);
+
+		const subnetMaskInverted = (~subnetMask) >>> 0;
+
+		// If subnet == 32, the only host address is the net address.
+		// If subnet == 31, the last host address is the last address in the address space.
+		// If subnet <= 30, the last host address is the IP address before the last address
+		// in the address space (which is the broadcast address).
+		let ipLast = (ipNetwork | subnetMaskInverted) >>> 0;
+		if (subnet <= 30) {
+			ipLast--;
+		}
+
+		return ipLast;
 	}
 }
 
