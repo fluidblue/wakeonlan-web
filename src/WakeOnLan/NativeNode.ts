1ceff6a824f1c79e78573922e97a4df9d301e2ba
fluidblue <fluidblue@users.noreply.github.com>
2021-08-03 01:14:21 +0200

    Rewrote function wake to be async

--- src/WakeOnLan/NativeNode.ts
+++ src/WakeOnLan/NativeNode.ts
@@ -29,51 +29,54 @@ function createMagicPacket(macAddress: Uint8Array): Uint8Array {
  * Details on Wake-on-LAN:
  * https://www.amd.com/system/files/TechDocs/20213.pdf
  */
-function wake(macAddress: Uint8Array, port: number = WAKE_ON_LAN_DEFAULT_PORT, address: string = BROADCAST_ADDRESS_IP4, protocol?: Protocol) {
-	let checkIP = net.isIP(address);
-	if (!checkIP) {
-		throw new Error("IP address not valid");
-	}
-	if (!protocol) {
-		if (checkIP == 6) {
-			protocol = "udp6";
-		} else {
-			protocol = "udp4";
+function wake(macAddress: Uint8Array, port: number = WAKE_ON_LAN_DEFAULT_PORT, address: string = BROADCAST_ADDRESS_IP4, protocol?: Protocol): Promise<void> {
+	return new Promise((resolve, reject) => {
+		let checkIP = net.isIP(address);
+		if (!checkIP) {
+			throw new Error("IP address not valid");
 		}
-	}
-
-	const magicPacket: Uint8Array = createMagicPacket(macAddress);
-
-	const socket = dgram.createSocket({
-		type: protocol,
-	});
-
-	socket.on("error", (err) => {
-		console.log(err);
-	});
-
-	socket.connect(port, address, () => {
-		// Set SO_BROADCAST socket option to allow sending to a broadcast address
-		socket.setBroadcast(true);
-
-		socket.send(magicPacket, 0, magicPacket.length, (err) => {
-			if (err) {
-				console.log(err);
+		if (!protocol) {
+			if (checkIP == 6) {
+				protocol = "udp6";
 			} else {
-				console.log("Message has been sent.")
+				protocol = "udp4";
 			}
-			socket.close();
+		}
+
+		const magicPacket: Uint8Array = createMagicPacket(macAddress);
+
+		const socket = dgram.createSocket({
+			type: protocol,
+		});
+
+		socket.on("error", (err) => {
+			reject(err);
+		});
+
+		socket.connect(port, address, () => {
+			// Set SO_BROADCAST socket option to allow sending to a broadcast address
+			socket.setBroadcast(true);
+
+			socket.send(magicPacket, 0, magicPacket.length, (err) => {
+				socket.close();
+				if (err) {
+					reject(err);
+					return;
+				}
+				console.log("Message has been sent.");
+				resolve();
+			});
 		});
 	});
 }
 
-function main() {
+async function main() {
 	const destinationMacAddress: Uint8Array = Buffer.alloc(MACFunctions.MAC_ADDR_LENGTH, "001FD0DB55A2", "hex");
 	const destinationPort: number = WAKE_ON_LAN_DEFAULT_PORT;
 
-	wake(destinationMacAddress);
-	wake(destinationMacAddress, 7);
-	wake(destinationMacAddress, 9, "192.168.188.22");
+	await wake(destinationMacAddress);
+	await wake(destinationMacAddress, 7);
+	await wake(destinationMacAddress, 9, "192.168.188.22");
 }
 
 main();
