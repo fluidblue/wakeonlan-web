45dc0bf76653d0657ca1c783a65f4efa418c1bef
fluidblue <fluidblue@users.noreply.github.com>
2021-08-03 00:33:41 +0200

    Fixed ping

--- src/HostDiscovery/Ping.ts
+++ src/HostDiscovery/Ping.ts
@@ -46,9 +46,11 @@ export class Ping {
 				}
 				if (error) {
 					reject(error);
+					return;
 				}
 				if (stderr && stderr.length > 0) {
 					reject(new Error(stderr));
+					return;
 				}
 				resolve();
 			});
