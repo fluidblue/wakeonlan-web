b84e3f02922a2028a97a268f7b6372a444efd1ff
fluidblue <fluidblue@users.noreply.github.com>
2021-08-04 02:24:17 +0200

    Revert "Removed callbackHostFound to speed up process"
    This reverts commit 7cb98cf604189a30e10a238d9ed7d53955da7b11.

--- src/HostDiscovery/ARPCacheAndPing.ts
+++ src/HostDiscovery/ARPCacheAndPing.ts
@@ -9,22 +9,23 @@ export default class ARPCacheAndPing implements HostDiscovery {
 
 	async discover(
 		ipSubnet: IPNetwork,
-		callbackProgress: (done: number, total: number) => void
+		callbackProgress: (done: number, total: number) => void,
+		callbackHostFound: (ipAddress: string, macAddress: MacAddressBytes) => void
 	): Promise<ARPCacheEntry[]> {
 		if (ipSubnet.prefix < 1 || ipSubnet.prefix > 32) {
 			throw new RangeError("IP prefix must be between 1 and 32.");
 		}
 
+		const hosts: ARPCacheEntry[] = [];
+		const runningPromises: Promise<ARPCacheEntry | null>[] = [];
+
 		const ipFirst: number = IPFunctions.getFirstAddress(ipSubnet);
 		const ipLast: number = IPFunctions.getLastAddress(ipSubnet);
 
-		const runningPromises: Promise<void>[] = [];
-
 		let lastRun = this.getTimeInMilliseconds();
 		for (let ip = ipFirst; ip <= ipLast; ip++) {
-			// Start pinging
-			const ipString = IPFunctions.getStringIP(ip);
-			const promise = Ping.ping(ipString);
+			// Start discovering host
+			const promise = this.discoverHost(ip);
 			runningPromises.push(promise);
 
 			// Calculate duration
@@ -40,28 +41,30 @@ export default class ARPCacheAndPing implements HostDiscovery {
 
 		// Wait for all promises to finish
 		for (let i = 0; i < runningPromises.length; i++) {
-			await runningPromises[i];
+			const host: ARPCacheEntry | null = await runningPromises[i];
+			if (host) {
+				hosts.push(host);
+				callbackHostFound(host.ip, MACFunctions.getByteArrayFromMacAddress(host.mac));
+			}
 			callbackProgress(i + 1, runningPromises.length);
 		}
-
-		// Get relevant hosts from ARP Cache
-		const hosts: ARPCacheEntry[] = await this.getARPCacheHosts(ipFirst, ipLast);
 
 		return hosts;
 	}
 
-	async getARPCacheHosts(ipFirst: IPAddressNumerical, ipLast: IPAddressNumerical): Promise<ARPCacheEntry[]> {
+	async discoverHost(ip: IPAddressNumerical): Promise<ARPCacheEntry | null> {
+		let ipString: string = IPFunctions.getStringIP(ip);
+
+		await Ping.ping(ipString);
 		const arpCache = await ARPCache.getARPCache();
 
-		const result: ARPCacheEntry[] = [];
 		for (let entry of arpCache) {
 			const numericEntryIP = IPFunctions.getNumericalIP(entry.ip);
-			if (numericEntryIP >= ipFirst && numericEntryIP <= ipLast) {
-				result.push(entry);
+			if (ip === numericEntryIP) {
+				return entry;
 			}
 		}
-
-		return result;
+		return null;
 	}
 
 	delay(timeout: number): Promise<void> {
