5cf63a3e71ea7ca23089cfeb051dffb104907cdd
fluidblue <fluidblue@users.noreply.github.com>
2021-09-01 15:33:29 +0200

    Added express skeleton

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,20 @@
  * ==================================================================
  */
  
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 8000;
+
+// Serve static files
+app.use("/", express.static(path.join(__dirname, "httpdocs")));
+
+// REST API
+app.get("/api", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(port, () => {
+	console.log(`wakeonlan-web listening at http://localhost:${port}`);
+});
