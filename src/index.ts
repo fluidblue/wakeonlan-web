a08a6e9cf55b3d9cd6aa2baf5dcec84db83d0620
fluidblue <fluidblue@users.noreply.github.com>
2021-09-01 17:41:54 +0200

    Implemented /api/wakeonlan

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,11 @@
 import express from "express";
 import path from "path";
 
+import { WakeOnLan } from "./WakeOnLan/WakeOnLan";
+import WolNativeNode from "./WakeOnLan/WolNativeNode";
+
+import { MACFunctions } from "./HostDiscovery/MACFunctions";
+
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -63,9 +68,24 @@ app.post("/api/host-discovery/arp-cache-and-ping", (req, res) => {
 
 app.post("/api/wakeonlan", (req, res) => {
 	const mac = req.body["mac"];
-	const port = req.body["port"];
-	const ip = req.body["ip"];
-	res.send("Not yet implemented.");
+	const port = req.body["port"] || WakeOnLan.DEFAULT_PORT;
+	const ip = req.body["ip"] || WakeOnLan.IP_BROADCAST_ADDRESS;
+
+	const options = {
+		port: port,
+		address: ip
+	}
+
+	const wolManager: WakeOnLan = new WolNativeNode();
+	wolManager.wake(MACFunctions.getByteArrayFromMacAddress(mac), options).catch((err) => {
+		res.send(JSON.stringify({
+			result: false
+		}));
+	}).then(() => {
+		res.send(JSON.stringify({
+			result: true
+		}));
+	});
 });
 
 app.listen(port, () => {
