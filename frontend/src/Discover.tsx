043471100b59bc1b6738cf8ac6d495e4a95332a9
fluidblue <fluidblue@users.noreply.github.com>
2021-09-07 12:42:11 +0200

    Using scanning state

--- frontend/src/Discover.tsx
+++ frontend/src/Discover.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Discover.css';
 
 function Discover() {
+  const [scanning, setScanning] = useState(true);
+
   function handleItemClick(e: React.MouseEvent<HTMLLIElement, MouseEvent>) {
     alert('savehost');
+  }
+
+  window.setTimeout(() => {
+    setScanning(false);
+  }, 3000);
+
+  let spinner = null;
+  if (scanning) {
+    spinner = (
+      <div className="d-flex align-items-center mt-3 mx-2 spinner">
+        <span className="fw-bold">Scanning 192.168.188.0/24</span>
+        <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+      </div>
+    );
+  }
+
+  let scanFinishedNotice = null;
+  if (!scanning) {
+    scanFinishedNotice = (
+      <div className="d-flex flex-column align-items-center mx-2 mt-4 scan-finished-notice">
+        <div>
+          <div className="fw-bold text-center">Scan finished.</div>
+        </div>
+        <div className="mt-3">
+          <div className="text-center mb-2">Host not found?</div>
+          <div className="d-flex">
+            <button type="button" className="btn btn-sm btn-secondary">Rescan</button>
+            <button type="button" className="btn btn-sm btn-secondary ms-2">Add manually</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -33,23 +67,9 @@ function Discover() {
         </li>
       </ul>
 
-      <div className="d-flex align-items-center mt-3 mx-2 spinner">
-        <span className="fw-bold">Scanning 192.168.188.0/24</span>
-        <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-      </div>
+      {spinner}
 
-      <div className="d-flex flex-column align-items-center mx-2 mt-4 scan-finished-notice d-none">
-        <div>
-          <div className="fw-bold text-center">Scan finished.</div>
-        </div>
-        <div className="mt-3">
-          <div className="text-center mb-2">Host not found?</div>
-          <div className="d-flex">
-            <button type="button" className="btn btn-sm btn-secondary">Rescan</button>
-            <button type="button" className="btn btn-sm btn-secondary ms-2">Add manually</button>
-          </div>
-        </div>
-      </div>
+      {scanFinishedNotice}
     </>
   );
 }
