bc5ace5d0abd0090585bba34eef011ba1ddef394
fluidblue <fluidblue@users.noreply.github.com>
2021-09-08 22:10:16 +0200

    Formatting

--- frontend/src/ToastContainer.tsx
+++ frontend/src/ToastContainer.tsx
@@ -44,6 +44,7 @@ function ToastContainer() {
   const toasts = hostnames.map((hostname) => {
     return <ToastWolPacketSent hostname={hostname} />;
   });
+
   return (
     <div id="toastContainer" className="toast-container position-fixed bottom-0 start-50 translate-middle-x p-3">
       {toasts}
