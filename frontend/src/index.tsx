1ef673dacdd1541bd59f56ff2abb63f63c16f75e
fluidblue <fluidblue@users.noreply.github.com>
2021-09-08 21:30:35 +0200

    Using bootstrap and bootstrap-icons

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
