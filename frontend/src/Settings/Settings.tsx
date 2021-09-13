23745279f8b9277632a0d9e88c2f8c73dc87f45a
fluidblue <fluidblue@users.noreply.github.com>
2021-09-13 14:01:58 +0200

    Added input and button handlers

--- frontend/src/Settings/Settings.tsx
+++ frontend/src/Settings/Settings.tsx
@@ -1,48 +1,104 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Settings.css';
 
-interface IPNetwork {
-  ip: string,
-  prefix: number
-}
-
-function ipNetworkToString(ipNetwork: IPNetwork): string {
-  return ipNetwork.ip + "/" + ipNetwork.prefix;
-}
+import { IPFunctions, IPNetwork } from 'wakeonlan-utilities';
 
 function ipNetworksToString(ipNetworks: IPNetwork[]): string {
-  let result = "";
-  let first: boolean = true;
-  for (const ipNetwork of ipNetworks) {
-    if (first) {
-      first = false;
-    } else {
-      result += ", ";
-    }
-    result += ipNetworkToString(ipNetwork);
-  }
-  return result;
+  const ipNetworkStrings = ipNetworks.map((value) => {
+    return IPFunctions.getStringFromIPNetwork(value);
+  });
+  return ipNetworkStrings.join(', ');
 }
 
-const ipNetworksAutoDetectedMock: IPNetwork[] = [
-  { ip: "192.168.178.0", prefix: 24 }
-];
+const WAKEONLAN_DEFAULT_PORT: number = 9;
 
 function Settings() {
-  const [ipNetworks, setIpNetworks] = useState<IPNetwork[]>(ipNetworksAutoDetectedMock);
-  const [autoDetectNetworks, setAutoDetectNetworks] = useState<boolean>(true);
+  const [ipNetworks, setIpNetworks] = useState<IPNetwork[]>([]);
+  const [ipNetworksAutoDetected, setIpNetworksAutoDetected] = useState<IPNetwork[]>([]);
 
-  function onCheckboxAutoDetectChange(e: React.ChangeEvent<HTMLInputElement>) {
-    if (e.target.checked) {
-      setIpNetworks(ipNetworksAutoDetectedMock);
+  const [ipNetworksString, setIpNetworksString] = useState<string>("");
+  const [autoDetectNetworks, setAutoDetectNetworks] = useState<boolean>(true);
+  const [wolPort, setWolPort] = useState<number>(WAKEONLAN_DEFAULT_PORT);
+
+  useEffect(() => {
+    // TODO: Fetch IP networks from server
+    const ipNetworksAutoDetectedMock: IPNetwork[] = [
+      { ip: "192.168.178.0", prefix: 24 },
+      { ip: "192.168.188.0", prefix: 24 }
+    ];
+    setIpNetworksAutoDetected(ipNetworksAutoDetectedMock);
+  }, []);
+
+  const resetIpNetworks = useCallback(() => {
+    setIpNetworks(ipNetworksAutoDetected);
+    setIpNetworksString(ipNetworksToString(ipNetworksAutoDetected));
+  }, [ipNetworksAutoDetected]);
+
+  useEffect(() => {
+    resetIpNetworks();
+  }, [resetIpNetworks]);
+
+  function setIpNetworkAutoDetection(value: boolean) {
+    if (value) {
+      setIpNetworks(ipNetworksAutoDetected);
+      setIpNetworksString(ipNetworksToString(ipNetworksAutoDetected));
       setAutoDetectNetworks(true);
     } else {
       setAutoDetectNetworks(false);
     }
   }
 
+  function onCheckboxAutoDetectChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setIpNetworkAutoDetection(e.target.checked);
+  }
+
+  function updateIpNetworks() {
+    let valid: boolean = true;
+    const ipNetworkStrings: string[] = ipNetworksString.split(',');
+    const ipNetworksNew: IPNetwork[] = [];
+    for (let ipNetworkString of ipNetworkStrings) {
+      ipNetworkString = ipNetworkString.trim();
+      let ipNetwork: IPNetwork;
+      try {
+        ipNetwork = IPFunctions.getIPNetworkFromString(ipNetworkString);
+      } catch (err) {
+        valid = false;
+        break;
+      }
+      ipNetworksNew.push(ipNetwork);
+    }
+    if (valid) {
+      setIpNetworks(ipNetworksNew);
+      console.log('valid, setIpNetworks', ipNetworksNew); // TODO: Remove
+    }
+    if (!valid) {
+      console.log('invalid', ipNetworksString); // TODO: Remove
+    }
+  }
+
   function onInputNetworkChange(e: React.ChangeEvent<HTMLInputElement>) {
-    console.log(e.target.value); // TODO
+    setIpNetworksString(e.target.value);
+  }
+
+  function onInputPortChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = parseInt(e.target.value);
+    if (isNaN(value)) {
+      // TODO: Set form invalid
+    } else {
+      setWolPort(value);
+    }
+  }
+
+  function onSave(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    // TODO
+    updateIpNetworks();
+  }
+
+  function onReset() {
+    setIpNetworkAutoDetection(true);
+    setWolPort(WAKEONLAN_DEFAULT_PORT);
   }
 
   return (
@@ -55,7 +111,7 @@ function Settings() {
             type="text"
             className="form-control"
             id="inputNetwork"
-            value={ipNetworksToString(ipNetworks)}
+            value={ipNetworksString}
             placeholder="Enter network, e.g. 192.168.178.0/24"
             required
             onChange={onInputNetworkChange}
@@ -83,14 +139,22 @@ function Settings() {
         <h6 className="mb-3 fw-bold">Wake on LAN</h6>
         <div className="mb-3">
           <label htmlFor="inputPort" className="form-label">UDP Port</label>
-          <input type="text" className="form-control" id="inputPort" value="9" placeholder="Enter port number" required />
+          <input
+            type="number"
+            className="form-control"
+            id="inputPort"
+            placeholder="Enter port number"
+            value={wolPort}
+            onChange={onInputPortChange}
+            required
+          />
         </div>
         <hr />
         <div className="mb-3">
-          <button type="button" className="btn btn-secondary">Cancel</button>
-          <button type="submit" className="btn btn-primary">Save</button>
+          <button type="button" className="btn btn-secondary" onClick={onReset}>Reset</button>
+          <button type="submit" className="btn btn-primary" onClick={onSave}>Save</button>
         </div>
-        </form>
+      </form>
     </div>
   );
 }
