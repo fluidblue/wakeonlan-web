3a5ab2577baff1bfd117b6579a0d4f174a2564c5
fluidblue <fluidblue@users.noreply.github.com>
2021-09-21 14:31:28 +0200

    Added tests for returning IAB and OUI organization names

--- backend/spec/OrganizationMappingSpec.ts
+++ backend/spec/OrganizationMappingSpec.ts
@@ -27,4 +27,16 @@ describe("OrganizationMapping", () => {
 		const initialized = await organizationMapping.isDBInitialized();
 		expect(initialized).toBeTrue();
 	});
+
+	it("should return an IAB organization name", async () => {
+		const organization = await organizationMapping.getOrganizationName("00:50:C2:7D:50:01");
+		expect(organization).toBeDefined();
+		expect(organization).toEqual("DEUTA-WERKE GmbH");
+	});
+
+	it("should return an OUI organization name", async () => {
+		const organization = await organizationMapping.getOrganizationName("00:22:72:00:00:01");
+		expect(organization).toBeDefined();
+		expect(organization).toEqual("American Micro-Fuel Device Corp.");
+	});
 });
