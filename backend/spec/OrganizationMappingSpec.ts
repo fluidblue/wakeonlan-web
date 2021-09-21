c9a6ac89c2b903c947a314aa72c14c8d323f2d54
fluidblue <fluidblue@users.noreply.github.com>
2021-09-21 11:41:55 +0200

    Increased test timeout

--- backend/spec/OrganizationMappingSpec.ts
+++ backend/spec/OrganizationMappingSpec.ts
@@ -3,12 +3,22 @@ import "jasmine";
 import Database from "../src/Database/Database";
 import OrganizationMapping from "../src/OrganizationMapping/OrganizationMapping"
 
+const timeout: number = 30; // in seconds
+
 describe("OrganizationMapping", () => {
+	let originalTimeout: number;
 	let organizationMapping: OrganizationMapping;
 
 	beforeEach(() => {
+		originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = timeout * 1000;
+
 		const database = new Database();
 		organizationMapping = new OrganizationMapping(database);
+	});
+
+	afterEach(() => {
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
 	});
 
 	it("should update the IAB and OUI list", async () => {
