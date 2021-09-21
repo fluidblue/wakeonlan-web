81de5ee406ec06e512b688ae179ebf5964d41285
fluidblue <fluidblue@users.noreply.github.com>
2021-09-21 10:40:04 +0200

    Updated OrganizationMapping

--- backend/src/OrganizationMapping/OrganizationMapping.ts
+++ backend/src/OrganizationMapping/OrganizationMapping.ts
@@ -1,23 +1,42 @@
+import fetch from "node-fetch";
+
 import Database from "../Database/Database";
+
+const URI_OUI = "http://standards-oui.ieee.org/oui/oui.txt";
+const URI_IAB = "http://standards-oui.ieee.org/iab/iab.txt";
 
 export default class OrganizationMapping {
 	private database: Database;
 
 	constructor(database: Database) {
 		this.database = database;
+
+		const that = this;
 		async function init() {
-			if (!await database.organizationMappingInitialized()) {
-				await database.organizationMappingIABUpdate();
-				await database.organizationMappingOUIUpdate();
+			if (!await that.isDBInitialized()) {
+				await that.updateIABList();
+				await that.updateOUIList();
 			}
 		}
 		init();
 	}
 
-	getOrganizationName(mac: string): Promise<string | null> {
-		let organization = this.database.organizationMappingIABGet(mac);
+	async updateIABList() {
+		// TODO
+	}
+
+	async updateOUIList() {
+		// TODO
+	}
+
+	async isDBInitialized(): Promise<boolean> {
+		return await this.database.organizationMappingInitialized();
+	}
+
+	async getOrganizationName(mac: string): Promise<string | null> {
+		let organization = await this.database.organizationMappingIABGet(mac);
 		if (!organization) {
-			organization = this.database.organizationMappingOUIGet(mac);
+			organization = await this.database.organizationMappingOUIGet(mac);
 		}
 		return organization;
 	}
