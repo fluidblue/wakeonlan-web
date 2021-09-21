cfb2a2721474b76120efc9cde6ca24bd72a032cf
fluidblue <fluidblue@users.noreply.github.com>
2021-09-21 12:10:26 +0200

    Added regular expressions

--- backend/src/OrganizationMapping/OrganizationMapping.ts
+++ backend/src/OrganizationMapping/OrganizationMapping.ts
@@ -8,6 +8,26 @@ const URI_IAB = "http://standards-oui.ieee.org/iab/iab.txt";
 export default class OrganizationMapping {
 	private database: Database;
 
+	/**
+	 * Matches entries in an IAB file.
+	 * 
+	 * Capturing groups:
+	 * Group 1: mac_part1
+	 * Group 2: organization (untrimmed)
+	 * Group 3: mac_part2_range_start
+	 * Group 4: mac_part2_range_end
+	 */
+	private static readonly RE_IAB = /([0-9A-Za-z]{1,2}(?:-[0-9A-Za-z]{1,2}){2})\s*\(hex\)\s*(.*)\n([0-9A-Za-z]{6})-([0-9A-Za-z]{6})\s*\(base 16\)/g;
+
+	/**
+	 * Matches entries in an OUI file.
+	 * 
+	 * Capturing groups:
+	 * Group 1: mac_part1
+	 * Group 2: organization
+	 */
+	private static readonly RE_OUI = /([0-9A-Za-z]{1,2}(?:-[0-9A-Za-z]{1,2}){2})\s*\(hex\)\s*(.*)\n/g;
+
 	constructor(database: Database) {
 		this.database = database;
 
