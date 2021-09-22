6c162289ee8f099e76e6ef745c24c4b5c5b7199d
fluidblue <fluidblue@users.noreply.github.com>
2021-09-22 16:55:58 +0200

    Compiling ts to js

--- backend/scripts/prepublish.sh
+++ backend/scripts/prepublish.sh
@@ -1,11 +1,14 @@
+# Frontend
 rm -rf src/httpdocs
-
 cd ../frontend
 rm -rf build
 yarn build
-
-cp -R build ../backend/src/httpdocs
+cp -R build ../backend/build/httpdocs
 cd ../backend
 
+# Docs
 cp ../*.md .
 cp -R ../docs ./docs
+
+# Backend
+tsc
