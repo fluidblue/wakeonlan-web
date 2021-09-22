381a607887dedd2337726f297bc7840ad8e77f35
fluidblue <fluidblue@users.noreply.github.com>
2021-09-22 10:14:57 +0200

    Updated start and test scripts

--- backend/development_test.sh
+++ backend/development_test.sh
@@ -1,4 +1,4 @@
-DATABASE_HOST="wol-debian" \
+DATABASE_HOST="raspberrypi" \
 DATABASE_DB="wakeonlan-web" \
 DATABASE_USER="wakeonlan-web" \
 DATABASE_PASSWORD_FILE="$(pwd)/../database_password.txt" \
