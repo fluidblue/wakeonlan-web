e9d8289322c27ec83f302eb677b3f00150740e61
fluidblue <fluidblue@users.noreply.github.com>
2021-09-24 07:20:32 +0200

    Fixed prepublish.sh

--- backend/scripts/prepublish.sh
+++ backend/scripts/prepublish.sh
@@ -1,13 +1,15 @@
 # Frontend
-rm -rf src/httpdocs
 cd ../frontend
-rm -rf build
+rm -r build
 yarn build
+rm -r ../backend/build/httpdocs
 cp -R build ../backend/build/httpdocs
 cd ../backend
 
 # Docs
+rm *.md
 cp ../*.md .
+rm -r ./docs
 cp -R ../docs ./docs
 
 # Backend
